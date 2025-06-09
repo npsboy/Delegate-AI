799521718a3e574bf63fcdcbbf055430ae809bad
Tushar V <tushar.vijayanagar@gmail.com>
2025-06-09 18:19:49 +0530

    added logrocket

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
 import reportWebVitals from './pages/reportWebVitals';
+import LogRocket from 'logrocket';
+LogRocket.init('ojatxp/delegate-ai');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
